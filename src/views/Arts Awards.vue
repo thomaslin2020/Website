128e58cebb6cf410259c1b79820caca2619285f6
Thomas <37830237+Thomasqazwsxedc@users.noreply.github.com>
2020-06-07 20:29:11 -0400

    Update Arts Awards.vue

--- src/views/Arts Awards.vue
+++ src/views/Arts Awards.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="parent">
         <div class="iframe-container">
-            <iframe v-if="windowWidth > 1200" width="640" height="360" src="https://www.youtube.com/embed/9lkFhNYrHCQ" frameborder="0"
+            <iframe v-if="windowWidth > 1200" width="640" height="360" src="https://www.youtube.com/embed/yqawEepsE4o" frameborder="0"
                     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" style="height: calc(100vh - 175px);"
                     allowfullscreen></iframe>
-            <iframe v-else width="640" height="360" src="https://www.youtube.com/embed/9lkFhNYrHCQ" frameborder="0"
+            <iframe v-else width="640" height="360" src="https://www.youtube.com/embed/yqawEepsE4o" frameborder="0"
                     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" style="height: 100%"
                     allowfullscreen></iframe>
         </div>
