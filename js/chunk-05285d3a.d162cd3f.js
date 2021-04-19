8c8cdf4518cea672185384b61eab9e55a8b60cac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-19 14:52:07 +0000

    Deploy thomaslin2020/ridley-arts-celebration to github.com/thomaslin2020/ridley-arts-celebration.git:gh-pages

---
+++ js/chunk-05285d3a.d162cd3f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-05285d3a"],{"21bb":function(n,t,a){"use strict";var e=a("2dad"),c=a.n(e);c.a},"2dad":function(n,t,a){},bb51:function(n,t,a){"use strict";a.r(t);var e=function(){var n=this,t=n.$createElement,a=n._self._c||t;return a("main",{staticClass:"main"})},c=[],s={name:"Home",components:{}},u=s,i=(a("21bb"),a("2877")),o=Object(i["a"])(u,e,c,!1,null,null,null);t["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-05285d3a.d162cd3f.js.map
